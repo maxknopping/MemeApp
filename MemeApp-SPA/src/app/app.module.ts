669a523e93f3fbddb9fb7b637c1f0ea41aa553db
Max Knopping <maxknopping@Maxs-MBP-3.fios-router.home>
2019-09-25 19:05:33 -0400

    Navbar, login, and register

--- MemeApp-SPA/src/app/app.module.ts
+++ MemeApp-SPA/src/app/app.module.ts
@@ -2,19 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
 import { HttpClient } from 'selenium-webdriver/http';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent
+      NavComponent,
+      HomeComponent,
+      RegisterComponent
    ],
    imports: [
       HttpClientModule,
-      BrowserModule
+      BrowserModule,
+      FormsModule
    ],
-   providers: [],
+   providers: [
+      AuthService
+   ],
    bootstrap: [
       AppComponent
    ]
--- MemeApp.API/Controllers/ValuesController.cs
+++ MemeApp.API/Controllers/ValuesController.cs
@@ -20,7 +20,8 @@ namespace MemeApp.API.Controllers
             this._context = context;
 
         }
-        // GET api/values
+        
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
