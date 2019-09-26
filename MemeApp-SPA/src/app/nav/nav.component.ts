65d188e0d0ab193c05bd6464fef89d11881a0b6f
Max Knopping <maxknopping@Maxs-MBP-3.fios-router.home>
2019-09-25 21:44:03 -0400

    third party stuff (themes, alertify)

--- MemeApp-SPA/src/app/nav/nav.component.ts
+++ MemeApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,28 +10,30 @@ import { AuthService } from '../_services/auth.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    public authService: AuthService,
+    private alertify: AlertifyService
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   login() {
-    this.authService.login(this.model)
-    .subscribe(next => {
-      console.log('Loggin in successfully');
-    }, error => {
-      console.log(error);
-    });
+    this.authService.login(this.model).subscribe(
+      next => {
+        this.alertify.success('logged in successfully');
+      },
+      error => {
+        this.alertify.error('failed to log in');
+      }
+    );
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    return this.authService.loggedIn();
   }
 
   logOut() {
     localStorage.removeItem('token');
-    console.log('Logged out successfully');
+    this.alertify.success('logged out successfully');
   }
-
 }
